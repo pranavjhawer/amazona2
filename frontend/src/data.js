26b15deb8200814c8536c85efc63a8a90676030f
pranavjhawer <pranav.jhawer007@gmail.com>
2020-12-25 22:41:22 +0530

    frontend

---
+++ frontend/src/data.js
@@ -0,0 +1,32 @@
+export default {
+    products:[{
+        _id:'1',
+        name:'Slim Shirt',
+        category:'Shirts',
+        image:'/images/d1.jpg',
+        price: 60,
+        brand:'Nike',
+        rating:4,
+        numReviews: 10
+    },
+    {
+        _id:'2',
+        name:'Fit Shirt',
+        category:'Shirts',
+        image:'/images/d1.jpg',
+        price: 65,
+        brand:'Nike',
+        rating:4.5,
+        numReviews: 10
+    },
+    {
+        _id:'3',
+        name:'Best Shirt',
+        category:'Pants',
+        image:'/images/d1.jpg',
+        price: 100,
+        brand:'Nike',
+        rating:5,
+        numReviews: 10
+    }]
+}
