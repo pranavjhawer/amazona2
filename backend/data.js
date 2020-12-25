a237134cedc4c44a4cf148ab0632924e5fc9ce5e
pranavjhawer <pranav.jhawer007@gmail.com>
2020-12-25 22:41:51 +0530

    amazona

---
+++ backend/data.js
@@ -0,0 +1,46 @@
+export default {
+    products:[{
+        _id:'1',
+        name:'Slim Shirt',
+        category:'Shirts',
+        image:'/images/d1.jpg',
+        price: 60,
+        brand:'Nike',
+        rating:4,
+        numReviews: 10,
+        countInStock:10
+    },
+    {
+        _id:'2',
+        name:'Fit Shirt',
+        category:'Shirts',
+        image:'/images/d1.jpg',
+        price: 65,
+        brand:'Nike',
+        rating:4.5,
+        numReviews: 10,
+        countInStock:5
+    },
+    {
+        _id:'3',
+        name:'Best Shirt',
+        category:'Pants',
+        image:'/images/d1.jpg',
+        price: 100,
+        brand:'Nike',
+        rating:5,
+        numReviews: 10,
+        countInStock:7
+    },
+    {
+        _id:'4',
+        name:'Anupam Shirt',
+        category:'TEE',
+        image:'/images/d1.jpg',
+        price: 107,
+        brand:'Bata',
+        rating:0,
+        numReviews: 10,
+        countInStock:2
+    }]
+}
